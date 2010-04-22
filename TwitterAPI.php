e2286276f269a746b3f35fa315fa099a8265954e
Moacir de Oliveira <moacirdeoliveira.eng@gmail.com>
2010-04-22 17:49:40 -0400

    New method home_timeline, implementing method search()
    * home_timeline - Shows the timeline of the user's home page
    * search - Twitter search
    
    Signed-off-by: Moacir de Oliveira <moacirdeoliveira.eng@gmail.com>

--- TwitterAPI.php
+++ TwitterAPI.php
@@ -23,7 +23,17 @@ class TwitterAPI {
         return $this->request($url);
     }
 
-    public function search($query) {}
+    public function home_timeline() {
+        $url = self::TWITTER_URL . 'statuses/home_timeline.json';
+        return $this->request($url);
+    }
+
+    public function search($query, $page = 1) {
+        $url = 'http://search.twitter.com/search.json';
+        $url .= '?q=' . urlencode($query);
+        $url .= $page > 1 ? '&page=' . $page : false;
+        return $this->request($url);
+    }
 
     private function request($url, $params = array(), $method = 'GET') {
         $context = stream_context_create(array(
