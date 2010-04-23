6a0fbf1bf98b899d9ecc371aad5cbb19ad5b60e2
Rodrigo Rezende <rodrigo@rznd.(none)>
2010-04-23 12:43:42 -0400

    Basic layout

--- application/views/layout.php
+++ application/views/layout.php
@@ -1,3 +1,69 @@
-<?php
-
-var_dump($this);
+<html>
+	<head>
+		<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+		<link rel="stylesheet" type="text/css" media="screen,projection,print" href="./style.css" />
+		<title>Guira</title>
+	</head>
+	<body>
+		<div id="main">
+			<div id="header">
+				<a href="#"><img src="./images/logo.png" border=0 /></a>
+			</div>
+			<div id="nav">
+				<a href="#">Home</a>
+			</div>
+			<div id="tweet">
+				<form action="">
+					<div>O que está acontecendo?</div>
+					<div><textarea cols="105" rows="2"></textarea></div>
+					<div id="submit"><input type="submit" value="tweet"/></div>
+				<form>
+			<div id="content">
+				<h4>Linha do Tempo</h4>
+				<hr>
+				<div class="tweet">
+					<div class="avatar"><a href="http://twitter.com/roudik/">
+						<img border=0 src="./images/roudrik.jpg"/></a>
+					</div>
+					<div class="tweet-data">
+						<a class="name" href="">@roudrik</a>
+						<span class="text">Teste de simulação de um tweet bla bla bla bla bla</span>
+						</br>
+						<span class="date">about 1 hour ago</span>
+						<span class="source">via web</span>
+					</div> 
+				</div>
+				<div class="clear"></div>
+				<div class="tweet">
+					<div class="avatar"><a href="http://twitter.com/roudik/">
+						<img border=0 src="./images/roudrik.jpg"/></a>
+					</div>
+					<div class="tweet-data">
+						<a class="name" href="">@roudrik</a>
+						<span class="text">Teste de simulação de um tweet</span>
+						</br>
+						<span class="date">about 1 hour ago</span>
+						<span class="source">via web</span>
+					</div> 
+				</div>
+				<div class="clear"></div>
+				<div class="tweet">
+					<div class="avatar"><a href="http://twitter.com/roudik/">
+						<img border=0 src="./images/roudrik.jpg"/></a>
+					</div>
+					<div class="tweet-data">
+						<a class="name" href="">@roudrik</a>
+						<span class="text">Teste de simulação de um tweet</span>
+						</br>
+						<span class="date">about 1 hour ago</span>
+						<span class="source">via web</span>
+					</div> 
+				</div>
+				<div class="clear"></div>
+				<hr>
+			</div>
+			<div id="footer">
+			</div>
+		</div>
+	<body>
+</html>
